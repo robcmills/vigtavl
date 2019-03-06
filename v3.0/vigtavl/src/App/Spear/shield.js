fbc584f03eafd5686c9770500524b84bbccac0ab
robcmills <rob@buildingconnected.com>
2019-03-05 21:03:04 -0800

    Update simultaneous attack rules and diagrams

---
+++ v3.0/vigtavl/src/App/Spear/shield.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SpearShieldCircle = () => {
+	return (
+		<div>
+		  <img
+		    style={{ height: '10rem' }}
+		  	src='img/spear-shield-circle.svg'
+		  	alt='spear-shield-circle'
+		  />
+		</div>
+	);
+}
+
+export default SpearShieldCircle
